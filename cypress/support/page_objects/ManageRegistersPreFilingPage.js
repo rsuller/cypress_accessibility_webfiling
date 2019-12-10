a37f5e1f129883731a8119faaa45f9f24f2d236f
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2019-12-10 11:55:34 +0000

    move directors ura information test

--- cypress/support/page_objects/ManageRegistersPreFilingPage.js
+++ cypress/support/page_objects/ManageRegistersPreFilingPage.js
@@ -9,6 +9,14 @@ class ManageRegistersPreFilingPage {
         cy.contains('Change location of this register').click();
     }
 
+    selectDirectorsUraRegister() {
+        //Click Directors link and then change register location button
+        cy.get(':nth-child(3) > .toggle-link').click();
+        //Check after clicking the link
+        cy.accessibilityCheck();
+        cy.get(':nth-child(3) > .hidden-text > .grid_4 > p > .button').click();
+    }
+
     selectSecretariesRegister() {
         //Click Secretaries link and then change register location button
         cy.get(':nth-child(2) > .toggle-link').click();
